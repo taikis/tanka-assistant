08e6eb6d273aaa0322d1bf73f88e7667c5b87165
taiki <turkey.31452@gmail.com>
2022-10-12 15:59:07 +0900

    CUIを追加

--- kuzushiji-generator/code/cui.py
+++ kuzushiji-generator/code/cui.py
@@ -1,7 +1,27 @@
+import kuzushiji
+
+
 def main():
     # 文字を受け取る
     text = input("短歌を入力してください: ")
+    generator = kuzushiji.Generator(
+        text,
+        font_path="./font/KouzanBrushFontSousyo.ttf",
+        image_path="./background/japanese-paper_00096.jpg",
+    )
+    generator.draw_text()
+    # 署名を入力してください
+    name = input("署名を入力してください: ")
+    if name != "":
+        generator.stamp(
+            name,
+            "./font/aoyagireisyosimo_ttf_2_01.ttf",
+        )
+    # ファイル名の入力
+    path = input("ファイル名を入力してください: ")
+    output_path = generator.save(path)
+    print("正常に出力しました。\n" + f"出力先: {output_path}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
